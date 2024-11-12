c71640a84438bf33234987c52f4c8a487c253c9a
Leandro G. Rosa <105340567+Leandromeda@users.noreply.github.com>
2024-11-12 10:50:36 -0300

    Update wifi.bat

--- wifi.bat
+++ wifi.bat
@@ -1,16 +1,17 @@
-REM Dev. by Leandromeda ~~~>
+REM Dev. by Leandromeda
 
-ECHO Mostra os filtros de permissão e bloqueio ativos
-NETSH wlan show filters
+REM ECHO Mostra os filtros de permissão e bloqueio ativos
+REM NETSH wlan show filters
 
 REM Adiciona um filtro de permissão (allow) ou bloqueio (block)
-NETSH wlan add filter permission=allow ssid=DEV networktype=infrastructure
+REM NETSH wlan add filter permission=allow ssid=DEV networktype=infrastructure
 
 REM Tenta estabelecer conexão no wifi e senha indicados
-NETSH wlan connect ssid=DEV password=senai@2024
+REM NETSH wlan connect ssid=DEV password=senai@2024
 
 REM Para remover um filtro
 REM NETSH wlan delete filter permission=block ssid="WLAN name" networktype=infrastructure
 
 REM SSID é o nome da rede wifi
 REM Password a senha
+REM Remova o comando de comentário (REM) das linhas que deseja executar!

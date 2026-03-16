df678d2855f512de5d1c5fbe9dbb5d70ba31e37c
Leandro G. Rosa <105340567+Leandromeda@users.noreply.github.com>
2026-03-16 08:22:04 -0300

    Update server variable to use domain format

--- TestarPortas.ps1
+++ TestarPortas.ps1
@@ -1,4 +1,4 @@
-$servidor = "IP_DO_SERVIDOR"
+$servidor = "DOMÍNIO/IP_DO_SERVIDOR"
 $portas = @(20, 21, 22, 47400, 47401)
 foreach ($porta in $portas) {
     Test-NetConnection -ComputerName $servidor -Port $porta
